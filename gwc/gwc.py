33a848dec473486893889b755a6bfaf67ae9eb73
b166erobot <bernard0freitas@outlook.com>
2022-08-15 22:46:18 -0300

    um pequeno problema arrumado

--- gwc/gwc.py
+++ gwc/gwc.py
@@ -201,7 +201,7 @@ class Janela:
         """Método que seta a variável _apagar para verdadeiro ou falso e apaga o texto do aluno."""
         self._apagar = not self._apagar
         self._aluno_texto.set_text('')
-        self.aluno_digitando(None)
+        self.aluno_digitando(self._aluno_texto)
 
     def _normalizar_imagem(self):
         """Método que volta a imagem ao padrão."""
